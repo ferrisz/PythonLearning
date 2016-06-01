123f415f81c56c56f024bb032ee8f6a0b123f38e
Ferris <fynet86@gmail.com>
2016-06-02 01:07:44 +0800

    add freememory

--- openfile1.py
+++ openfile1.py
@@ -4,3 +4,19 @@
 fd = open('/tmp/tmp.txt')
 for line in fd.readlines():
     print line,
+
+for line in fd:
+    print line,
+
+fd.close()
+
+
+with open('/tmp/tmp.txt') as fd:
+# fd = open('/tmp/tmp.txt')
+    while True:
+        line = fd.readline()
+        if not line:
+            break
+        print line,
+# fd.close()
+
