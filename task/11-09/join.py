1fe51f479190db9010fddc5471179c9726375d3a
Ferris <fynet86@gmail.com>
2016-11-09 20:14:48 +0800

    修改了list的提示语句，完善join

---
+++ task/11-09/join.py
@@ -0,0 +1,13 @@
+#!/usr/local/python
+# coding=utf-8
+# Created by Ferris on 2016/11/9
+
+string = []
+
+while 1 :
+    string_temp = raw_input('请输入字符串, 如想结束输入请单独输入" . ":')
+    if string_temp == '.':
+        break
+    string.append(string_temp)
+
+print ' '.join(string) + '.'
--- task/11-09/list.py
+++ task/11-09/list.py
@@ -1,7 +1,8 @@
 #!/usr/local/python
 # coding=utf-8
+# Created by Ferris on 2016/11/9
 
-i = raw_input('Please input a string:')
+i = raw_input('请输入一个字符串')
 b = list(i)
 for a in b:
     print a
