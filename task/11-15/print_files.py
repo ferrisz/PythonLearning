dc210caf2c140142ec263e426f25e1acd5bcfe88
Ferris <fynet86@gmail.com>
2016-11-16 00:23:10 +0800

    完成11-15作业

---
+++ task/11-15/print_files.py
@@ -0,0 +1,18 @@
+#!/usr/local/python
+# coding=utf-8
+# Created by Ferris on 2016/11/16
+
+import os, sys
+def print_files(path):
+    lsdir = os.listdir(path)
+    dirs = [i for i in lsdir if os.path.isdir(os.path.join(path, i))]
+    files = [i for i in lsdir if os.path.isfile(os.path.join(path, i))]
+    if files:
+        for f in files:
+            print os.path.join(path, f)
+    if dirs:
+        for d in dirs:
+            print_files(os.path.join(path, d))
+
+if __name__ == '__main__':
+    print_files(sys.argv[1])
