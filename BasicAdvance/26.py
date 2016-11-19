a2f3b483ffda7a6b5d8f087d8fa82ae24754496a
Ferris <fynet86@gmail.com>
2016-11-19 21:26:52 +0800

    完成模块使用（二）的学习

---
+++ BasicAdvance/26.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding=utf-8
+# Created by Ferris on 2016/11/19
+
+# 模块使用（二）
+
+
+# 使用if __name__ == '__main__': 时模块执行可输出，被调用的时候不输出
+# 这样的目的时可以之后被其他脚本所调用
+
+
+# 总结
+模块时一个可以导入的python脚本文件
+包时一些按目录组织的模块和子包，目录下有__init__.py，此文件可以存包的信息
+# 导入模块和包的语法
+#   import, import as
+#   from ... import ..
