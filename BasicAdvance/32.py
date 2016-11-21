1dbfac4d8720d18f83505cff6b01e3df82c418da
Ferris <fynet86@gmail.com>
2016-11-21 18:57:01 +0800

    学习类的继承（二）

---
+++ BasicAdvance/32.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding=utf-8
+# Created by Ferris on 2016/11/21
+
+# 类的继承（二）
+
+# 多重继承
+# Python 支持多重继承，即一个类可以继承多个父类：
+# 语法：
+# class class_name(Parent_c1, Parent_c2)
+# 注意：
+# 当父类中出现多个自定义的__init__方法时，多重继承只执行第一个类的__init__方法，其他的不执行
+
