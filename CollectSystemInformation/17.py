2fdfe77d4357ce1fec521319b5d826c1b4d98c22
Ferris <fynet86@gmail.com>
2016-11-29 22:52:45 +0800

    python 异常处理

---
+++ CollectSystemInformation/17.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding=utf-8
+# Created by Ferris on 2016/11/29
+
+# python 异常处理
+# NameError 尝试访问一个没有声明的变量
+# ImportError 无法引入模块或包
+# IndentationError 语法错误（的子类），代码没有正确的对齐
+# SyntaxError 语法错误
+# IndexError 索引超出序列范围
+# KeyError 请求一个不存在的字典关键字
+# IOError 输入输出错误（比如你要读的文件不存在）
+# AttributeError 尝试访问位置的对象属性
+# ValueError 传给函数的参数类型不正确，比如给int()函数传入字符串类型
+# UnboundLocalError 师徒访问一个还未被设置的局部变量 可以使用global 解决
+# KeyboardInterrupt 键盘终端异常 脚本执行的时候按ctrl-c 的报的异常
