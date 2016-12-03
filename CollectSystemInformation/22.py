950b4b21232d449c5c65efe3ca693de6a11e0949
Ferris <fynet86@gmail.com>
2016-12-04 00:41:28 +0800

    Dmidecode系统信息

--- CollectSystemInformation/22.py
+++ CollectSystemInformation/22.py
@@ -7,6 +7,7 @@ from subprocess import PIPE, Popen
 p = Popen(['dmidecode'], stdout=PIPE)
 data = p.stdout
 lines = []
+dmi = {}
 a = True
 while a:
     line = data.readline()
@@ -19,5 +20,10 @@ while a:
             else:
                 lines.append(line)
 
-print s\
-    # mark
+dmi_dic = dict([i.strip().split(':') for i in lines])
+for k, v in dmi_dic.items():
+    dmi[k] = v.strip()
+dmi['Manufacturer'] = dmi_dic['Manufacturer'].strip()
+dmi['Product'] = dmi_dic['Product Name'].strip()
+dmi['Serial'] = dmi_dic['Serial Number'].strip()
+print dmi
