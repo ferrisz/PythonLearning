d91fa181c2f59f76abf298d3d1e41792fe9bb03a
Ferris <fynet86@gmail.com>
2016-12-06 11:26:56 +0800

    收集主机信息到cmdb

---
+++ Django/17.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding=utf-8
+# Created by Ferris on 2016/12/6
+
+
+# 收集主机信息到cmdb
+
+# urllib, urllib2, httplib
+# 使用urllib访问网页和传数据
+# req = urllib2.urloprn('http://127.0.0.1:8000/hostinfo/collect/')
+# 对字典data进行编码，生成post格式的数据
+# d = urllib.urlencode(data)
+# req = urllib2.urlopen('http://127.0.0.1:8000/hostinfo/collect/',d)
